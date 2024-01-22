83f94f1bfba2fc676276c72b450dcf0d921bda14
Aкѕнαу <akshaypmna18@gmail.com>
2024-01-22 14:48:38 +0530

    feat: :sparkles: implement theme feature
    This commit includes all the necessary changes to introduce the theme feature. The implementation covers the required modifications across the codebase to support and integrate the new theme functionality.

---
+++ src/features/theme/Theme.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Theme from "./Theme";
+
+export default {
+  title: "Features/Theme",
+  Component: Theme,
+};
+
+export const Component = (args) => <Theme {...args} />;
---
+++ src/features/theme/Theme.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
+
+export default function Theme() {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
