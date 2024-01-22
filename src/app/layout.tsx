edbccbed463ea8ec60ab41f5bf8486f25ed26dbd
Aкѕнαу <akshaypmna18@gmail.com>
2024-01-22 23:12:50 +0530

    feat: :art: separate metadata into dedicated file
    It involves the separation of metadata from the main layout file into a dedicated file. This restructuring enhances the organization and maintainability of the codebase by isolating metadata concerns into a distinct file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/features/theme/ThemeProvider";
+import meta from "@/contants/metadata";
 
 // const poppins = Poppins({
 //   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -9,93 +9,7 @@ import ThemeProvider from "@/features/theme/ThemeProvider";
 // });
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Akshay K - Personal Portfolio",
-  description:
-    "Passionate and versatile web developer creating responsive and user-friendly websites. Explore my portfolio to see a showcase of projects and Skills in front-end technologies",
-  icons: {
-    icon: "/icon.png",
-    shortcut: "/icon1.png",
-    apple: "/apple-icon.png",
-    // other: {
-    //   rel: 'apple-touch-icon-precomposed',
-    //   url: '/apple-touch-icon-precomposed.png',
-    // },
-  },
-  keywords: [
-    "Akshay",
-    "Akshay K",
-    "Akshaypmna",
-    "Akshay Portfolio",
-    "Akshay K Portfolio",
-    "Akshaypmna Portfolio",
-    "Akshay Perinthalmanna",
-  ],
-  creator: "Akshay K",
-  authors: [{ name: "Akshay K" }],
-  applicationName: "Akshay K - Personal Portfolio",
-  category: "Portfolio website",
-  publisher: "Akshay K",
-  verification: {
-    google: "google",
-    yandex: "yandex",
-    yahoo: "yahoo",
-    other: {
-      me: ["akshaypmnacoc@gmail.com", "https://aks-dev.vercel.app"],
-    },
-  },
-  openGraph: {
-    title: "Akshay K - Personal Portfolio",
-    description:
-      "Passionate and versatile web developer creating responsive and user-friendly websites. Explore my portfolio to see a showcase of projects and Skills in front-end technologies",
-    type: "website",
-    url: "https://aks-dev.vercel.app",
-    siteName: "Akshay K - Personal Portfolio",
-    images:
-      "https://github.com/Akshaypmna18/portfolio/blob/master/src/app/icon1.png",
-    countryName: "India",
-    emails: "akshaypmna18@gmail.com",
-    phoneNumbers: "+91 9495956795",
-  },
-  twitter: {
-    title: "Akshay K - Personal Portfolio",
-    description:
-      "Passionate and versatile web developer creating responsive and user-friendly websites. Explore my portfolio to see a showcase of projects and Skills in front-end technologies",
-    images:
-      "https://github.com/Akshaypmna18/portfolio/blob/master/src/app/icon1.png",
-    creator: "@akshaypmna18",
-  },
-  appleWebApp: {
-    statusBarStyle: "black-translucent",
-    title: "Akshay K - Personal Portfolio",
-  },
-  appLinks: {
-    web: {
-      url: "https://aks-dev.vercel.app",
-    },
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  metadataBase: new URL("https://aks-dev.vercel.app"),
-  alternates: {
-    canonical: "/",
-    languages: {
-      "en-US": "/en-US",
-      "de-DE": "/de-DE",
-    },
-  },
-};
+export const metadata = meta;
 
 export default function RootLayout({
   children,
@@ -104,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className={inter.className}>
+      <body
+        suppressHydrationWarning
+        className={`${inter.className} scroll-smooth`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
---
+++ src/contants/metadata.ts
@@ -0,0 +1,91 @@
+import type { Metadata } from "next";
+
+const meta: Metadata = {
+  title: "Akshay K - Personal Portfolio",
+  description:
+    "Passionate and versatile web developer creating responsive and user-friendly websites. Explore my portfolio to see a showcase of projects and Skills in front-end technologies",
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/icon1.png",
+    apple: "/apple-icon.png",
+    // other: {
+    //   rel: 'apple-touch-icon-precomposed',
+    //   url: '/apple-touch-icon-precomposed.png',
+    // },
+  },
+  keywords: [
+    "Akshay",
+    "Akshay K",
+    "Akshaypmna",
+    "Akshay Portfolio",
+    "Akshay K Portfolio",
+    "Akshaypmna Portfolio",
+    "Akshay Perinthalmanna",
+  ],
+  creator: "Akshay K",
+  authors: [{ name: "Akshay K" }],
+  applicationName: "Akshay K - Personal Portfolio",
+  category: "Portfolio website",
+  publisher: "Akshay K",
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      me: ["akshaypmnacoc@gmail.com", "https://aks-dev.vercel.app"],
+    },
+  },
+  openGraph: {
+    title: "Akshay K - Personal Portfolio",
+    description:
+      "Passionate and versatile web developer creating responsive and user-friendly websites. Explore my portfolio to see a showcase of projects and Skills in front-end technologies",
+    type: "website",
+    url: "https://aks-dev.vercel.app",
+    siteName: "Akshay K - Personal Portfolio",
+    images:
+      "https://github.com/Akshaypmna18/portfolio/blob/master/src/app/icon1.png",
+    countryName: "India",
+    emails: "akshaypmna18@gmail.com",
+    phoneNumbers: "+91 9495956795",
+  },
+  twitter: {
+    title: "Akshay K - Personal Portfolio",
+    description:
+      "Passionate and versatile web developer creating responsive and user-friendly websites. Explore my portfolio to see a showcase of projects and Skills in front-end technologies",
+    images:
+      "https://github.com/Akshaypmna18/portfolio/blob/master/src/app/icon1.png",
+    creator: "@akshaypmna18",
+  },
+  appleWebApp: {
+    statusBarStyle: "black-translucent",
+    title: "Akshay K - Personal Portfolio",
+  },
+  appLinks: {
+    web: {
+      url: "https://aks-dev.vercel.app",
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  metadataBase: new URL("https://aks-dev.vercel.app"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "de-DE": "/de-DE",
+    },
+  },
+};
+
+export default meta;
