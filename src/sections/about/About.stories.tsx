ecb8db58a34aa66f495b1bb28a2d3674e1ac563f
Aкѕнαу <akshaypmna18@gmail.com>
2024-01-23 23:15:26 +0530

    feat: create about section

---
+++ src/sections/about/About.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import About from "./About";
+
+export default {
+  title: "Sections/About",
+  Component: About,
+};
+
+export const Component = (args: any) => <About {...args} />;
