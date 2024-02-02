dd0d512a4603b91a7d7fb2373def60433ac42a94
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-02-02 18:22:52 +0530

    fix: 🐛 try resolve deployment issue

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -19,7 +19,9 @@ export const AnimatedTooltip = ({
     value: number;
   }[];
 }) => {
-  const [hoveredIndex, setHoveredIndex] = useState<string | null>(null);
+  const [hoveredIndex, setHoveredIndex] = useState<StaticImageData | null>(
+    null
+  );
   const springConfig = { stiffness: 100, damping: 5 };
   const x = useMotionValue(0); // going to set this value on mouse move
   // rotate the tooltip
