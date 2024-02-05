10e952d5ca82d72a55af3bb3cd25acd47b79abee
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-02-05 13:27:07 +0530

    refactor: update theme change logic

--- src/features/theme/Theme.tsx
+++ src/features/theme/Theme.tsx
@@ -3,43 +3,28 @@
 import * as React from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-} from "@/components/ui/dropdown-menu";
 
 export default function Theme({ className }: { className?: string }) {
-  const { setTheme, theme } = useTheme();
+  const { setTheme, theme, resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="bg-transparent border-0 hover:border hover:text-primaryColor"
-          size="icon"
-        >
-          <SunIcon
-            className={` rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ${className}`}
-          />
-          <MoonIcon
-            className={`absolute  rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 ${className}`}
-          />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      className="bg-transparent border-0 hover:border hover:text-primaryColor"
+      size="icon"
+      onClick={() => {
+        setTheme(isDarkMode ? "light" : "dark");
+      }}
+    >
+      <SunIcon
+        className={` rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ${className}`}
+      />
+      <MoonIcon
+        className={`absolute  rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 ${className}`}
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
