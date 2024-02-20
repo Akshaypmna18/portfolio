334d6bed57de6f0df02f0b7b4190770132bd4e49
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-02-20 13:21:15 +0530

    chore: minor changes

--- src/app/(projects)/hotelier-pro/HotelierPro.tsx
+++ src/app/(projects)/hotelier-pro/HotelierPro.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import img1 from "@/assets/projects/hotelier-pro/bookingpage.png";
-import img2 from "@/assets/projects/hotelier-pro/contact-section.png";
 import img3 from "@/assets/projects/hotelier-pro/features-section.png";
 import img4 from "@/assets/projects/hotelier-pro/footer.png";
 import img5 from "@/assets/projects/hotelier-pro/forms.png";
@@ -10,7 +9,7 @@ import img6 from "@/assets/projects/hotelier-pro/homepage.png";
 import img8 from "@/assets/projects/hotelier-pro/landing-page.png";
 import img9 from "@/assets/projects/hotelier-pro/pricing-section.png";
 
-const images = [img1, img2, img3, img4, img5, img6, img8, img9];
+const images = [img1, img3, img4, img5, img6, img8, img9];
 
 function HotelierPro() {
   const [loaded, setLoaded] = useState(false);
@@ -18,18 +17,29 @@ function HotelierPro() {
     <main className="xl:px-[20svh] min-h-[100svh] pt-12 pb-4 px-4 space-y-4">
       <h1 className="text-3xl font-bold text-center">Hotelier Pro</h1>
       <p>
-        I played a crucial role in the HotelierPro project as part of the
-        Woxware team, contributing significantly to the development of a
-        user-friendly platform that streamlines hotel operations and management
-        tasks.
+        I played a crucial role in the{" "}
+        <span className="font-semibold tracking-wider">HotelierPro</span>{" "}
+        project as part of the
+        <span className="font-semibold tracking-wider"> Woxware</span> team,
+        contributing significantly to the development of a user-friendly
+        platform that streamlines hotel operations and management tasks.
         <br />
-        <br /> With a focus on frontend development, I implemented Storybook for
+        <br /> With a focus on{" "}
+        <span className="font-semibold tracking-wider">
+          front end development
+        </span>
+        , I implemented{" "}
+        <span className="font-semibold tracking-wider">Storybook</span> for
         systematic UI component testing, enhancing code stability. Additionally,
-        I utilized Vitest and React Testing Library to ensure the reliability of
-        functions and components. Employing technologies such as React Hook
-        Forms, React Table, React Scheduler, and Zod schemas, I designed
-        components like forms and tables, improving user interaction and
-        optimizing dashboard functionality. <br />
+        I utilized <span className="font-semibold tracking-wider">Vitest</span>{" "}
+        and{" "}
+        <span className="font-semibold tracking-wider">
+          React Testing Library
+        </span>{" "}
+        to ensure the reliability of functions and components. Employing
+        technologies such as React Hook Forms, React Table, React Scheduler, and
+        Zod schemas, I designed components like forms and tables, improving user
+        interaction and optimizing dashboard functionality. <br />
         <br />
         Below are some pages, figma designs, stories[storybook files] related to
         the project.
--- src/app/(projects)/hotelier-pro/page.tsx
+++ src/app/(projects)/hotelier-pro/page.tsx
@@ -3,9 +3,6 @@ import hotelierProLogo from "@/assets/projects/hotelier-pro/hotelierProLogo.png"
 export const metadata = {
   title: "Hotelier Pro - Project",
   description: "Private Contribution",
-  icons: {
-    icon: "https://stage.hotelierpro.us/assets/images/favicon.png",
-  },
 };
 
 export { default } from "./HotelierPro";
