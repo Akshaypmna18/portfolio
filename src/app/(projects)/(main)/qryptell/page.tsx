a30d3184e369634b11b3cfb37849ea7a334a5718
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-03-09 20:10:25 +0530

    chore: restore accidentally removed metadata

--- src/app/(projects)/(main)/qryptell/page.tsx
+++ src/app/(projects)/(main)/qryptell/page.tsx
@@ -1,1 +1,9 @@
+export const metadata = {
+  title: "Qryptell - Project",
+  description: "Open Source Contribution",
+  icons: {
+    icon: "https://cdn-icons-png.flaticon.com/512/906/906361.png",
+  },
+};
+
 export { default } from "@/sections/projects/main/qryptell";
