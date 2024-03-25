e0b060049a8dc9702bdb500ad0fd38df13734e09
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-03-25 13:49:00 +0530

    feat: add loading delay

--- src/sections/projects/small/SmallProjects.tsx
+++ src/sections/projects/small/SmallProjects.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import ProjectCard from "./ProjectCard";
 import { projectsData } from "@/lib/projects/smallProjects";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ProjectData } from "@/lib/types";
 
 type ToggleType = ProjectData["category"] | "All";
@@ -11,6 +11,7 @@ type ToggleType = ProjectData["category"] | "All";
 /** Small Projects */
 function SmallProjects() {
   const [toggle, setToggle] = useState<ToggleType>("All");
+  const [isLoading, setIsLoading] = useState(true);
   const toggleBtn = "font-bold tracking-wider hover:bg-primary underline";
   const categories: ToggleType[] = [
     "All",
@@ -19,6 +20,12 @@ function SmallProjects() {
     "UI/UX",
     "Mobile Apps",
   ];
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => clearTimeout(delay);
+  }, []);
 
   return (
     <section className="p-4 pt-12 2xl:max-w-[80vw] 2xl:mx-auto relative">
@@ -37,20 +44,28 @@ function SmallProjects() {
           );
         })}
       </header>
-      <div className="mt-[8rem] min-[310px]:mt-20 min-[520px]:mt-12 sm:mt-16 flex flex-wrap gap-x-6 gap-y-8 xl:gap-x-8 xl:gap-y-12 justify-center items-center">
-        {toggle === "All" &&
-          projectsData.map((project, index) => (
-            <ProjectCard key={index} {...project} />
-          ))}
-        {toggle !== "All" &&
-          toggle !== "Mobile Apps" &&
-          projectsData
-            .filter((project) => project.category === toggle)
-            .map((project, index) => <ProjectCard key={index} {...project} />)}
-        {toggle === "Mobile Apps" && (
-          <p className="text-xl font-bold">Coming soon...</p>
-        )}
-      </div>
+      {isLoading ? (
+        <p className="mt-[8rem] min-[310px]:mt-20 min-[520px]:mt-12 sm:mt-16 text-center text-xl font-bold">
+          Loading...
+        </p>
+      ) : (
+        <main className="mt-[8rem] min-[310px]:mt-20 min-[520px]:mt-12 sm:mt-16 flex flex-wrap gap-x-6 gap-y-8 xl:gap-x-8 xl:gap-y-12 justify-center items-center">
+          {toggle === "All" &&
+            projectsData.map((project, index) => (
+              <ProjectCard key={index} {...project} />
+            ))}
+          {toggle !== "All" &&
+            toggle !== "Mobile Apps" &&
+            projectsData
+              .filter((project) => project.category === toggle)
+              .map((project, index) => (
+                <ProjectCard key={index} {...project} />
+              ))}
+          {toggle === "Mobile Apps" && (
+            <p className="text-xl font-bold">Coming soon...</p>
+          )}
+        </main>
+      )}
     </section>
   );
 }
